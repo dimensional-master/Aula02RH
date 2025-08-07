f17f41d4d685b3d84b05c256ffa45429e83a7d60
rafaela-oliveira21 <rafalelaoliveira20@gmail.com>
2025-08-06 22:36:19 -0300

    Aula02RH

---
+++ Models/Funcionario.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Aula02RH.Models.Enuns;
+
+namespace Aula02RH.Models
+{
+    public class Funcionario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public DateTime DataAdmissao { get; set; }
+        public decimal Salario { get; set; }
+        public TipoFuncionarioEnum TipoFuncionario { get; set; }
+
+        public void ReajustarSalario()
+        {
+            Salario = Salario + (Salario*10/100);
+        }
+
+        public string ExibirPeriodoExperiencia()
+        {
+            string periodoExperiencia 
+            = string.Format("Período de Experiência : {0} até {1}", 
+                DataAdmissao, DataAdmissao.AddMonths(3));
+                return periodoExperiencia;
+        }
+
+        public decimal CalcularDescontoVT(decimal percentual)
+        {
+            decimal desconto = this.Salario * percentual/100;
+            return desconto;
+        }
+
+    }
+}
